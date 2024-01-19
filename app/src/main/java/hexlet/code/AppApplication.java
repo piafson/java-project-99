2124db3bec8a34a792e328cf0deb19e9e7cbb492
piafson <pavorobyov@yandex.ru>
2024-01-19 13:26:05 +0800

    fix_TaskMapper

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AppApplication.class, args);
+
     }
 }
