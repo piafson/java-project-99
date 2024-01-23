d0ea3e638cb039cd5d6bbfbdcdce9a3477c86223
piafson <pavorobyov@yandex.ru>
2024-01-23 08:53:01 +0800

    fix_date_in_dto_2

--- src/main/java/hexlet/code/dto/LabelDTO.java
+++ src/main/java/hexlet/code/dto/LabelDTO.java
@@ -1,11 +1,13 @@
 package hexlet.code.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.openapitools.jackson.nullable.JsonNullable;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Data
 @AllArgsConstructor
@@ -13,5 +15,6 @@ import java.util.Date;
 public class LabelDTO {
     private JsonNullable<Long> id;
     private JsonNullable<String> name;
-    private JsonNullable<Date> createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private JsonNullable<LocalDate> createdAt;
 }
